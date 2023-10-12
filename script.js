36211f789e5f00be3cf0f07cad64ca66510048db
Rashmitha79 <134279042+Rashmitha79@users.noreply.github.com>
2023-10-12 14:30:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
+function numberOfBooksRead(library) {
+  // Check if the library object is defined and has a books property
+  if (library && library.books) {
+    // Use the filter function to get an array of books with readingStatus true
+    const readBooks = library.books.filter(book => book.readingStatus === true);
 
-const numberOfBooksRead = () => {
-  // write your code here
+    // Return the count of read books
+    return readBooks.length;
+  } else {
+    // Return 0 if the library object or books property is missing
+    return 0;
+  }
+}
+
+// Example usage:
+const library = {
+  books: [
+    { author: 'Author1', title: 'Book1', readingStatus: true },
+    { author: 'Author2', title: 'Book2', readingStatus: false },
+    { author: 'Author3', title: 'Book3', readingStatus: true }
+    // Add more books as needed
+  ]
 };
 
-// Do not change the code below
-
-alert(numberOfBooksRead());
+const result = numberOfBooksRead(library);
+console.log(result); // Output: 2
